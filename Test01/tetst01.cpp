8379dd33a114b86e42dfc6eb8fb98cce41e63ce6
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-06-07 18:02:01 +0300

    test01

---
+++ Test01/tetst01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void change(int value);
+
+int main() {
+	int number = 10;
+
+	cout << "before: number = " << number << "\n";
+
+	change(number);
+
+	cout << "after: number = " << number << "\n";
+
+	return 0;
+}
+
+void change(int value) {
+	value *= 2;
+
+	cout << "in function change: number = " << value << "\n";
+
+
+}
