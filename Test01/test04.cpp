67f4527c1e688b27da1eed52a3bafa69ab2413c1
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-06-07 18:51:08 +0300

    test05

--- Test01/test04.cpp
+++ Test01/test04.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-using namespace std;
-
-
-int main() {
-	bool* flag;
-	char* ch;
-	short* sh;
-	int* integer;
-	float* f;
-	long long* l;
-	double* d;
-	string* s;
-	void* v;
-
-	cout << "size of bool*: " << sizeof(flag) << " byte.\n";
-	cout << "size of char*: " << sizeof(ch) << " bytes.\n";
-	cout << "size of short*: " << sizeof(sh) << " bytes.\n";
-	cout << "size of int*: " << sizeof(integer) << " bytes.\n";
-	cout << "size of float*: " << sizeof(f) << " bytes.\n";
-	cout << "size of long long*: " << sizeof(l) << " bytes.\n";
-	cout << "size of double*: " << sizeof(d) << " bytes.\n";
-	cout << "size of string*: " << sizeof(s) << " bytes.\n";
-	cout << "size of void*: " << sizeof(v) << " bytes.\n";
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main() {
+//	bool* flag;
+//	char* ch;
+//	short* sh;
+//	int* integer;
+//	float* f;
+//	long long* l;
+//	double* d;
+//	string* s;
+//	void* v;
+//
+//	cout << "size of bool*: " << sizeof(flag) << " byte.\n";
+//	cout << "size of char*: " << sizeof(ch) << " bytes.\n";
+//	cout << "size of short*: " << sizeof(sh) << " bytes.\n";
+//	cout << "size of int*: " << sizeof(integer) << " bytes.\n";
+//	cout << "size of float*: " << sizeof(f) << " bytes.\n";
+//	cout << "size of long long*: " << sizeof(l) << " bytes.\n";
+//	cout << "size of double*: " << sizeof(d) << " bytes.\n";
+//	cout << "size of string*: " << sizeof(s) << " bytes.\n";
+//	cout << "size of void*: " << sizeof(v) << " bytes.\n";
+//
+//	return 0;
+//}
---
+++ Test01/test05.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//pointer - ptr
+int main() {
+	int* ptr_number = new int;
+	double* ptr_cost = new double;
+
+
+	*ptr_number = 10;
+	*ptr_cost = 2.5;
+
+	cout << "size of int*: " << sizeof(ptr_number) 
+		<< " bytes, @  = " << ptr_number 
+		<< ", value = " << *ptr_number << ".\n";
+
+	cout << "size of double*: " << sizeof(ptr_cost)
+		<< " bytes, @  = " << ptr_cost
+		<< ", value = " << *ptr_cost << ".\n";
+
+	delete ptr_number;
+	delete ptr_cost;
+
+	return 0;
+}
