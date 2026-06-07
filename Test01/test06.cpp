98ab048038656d0d447b56c1444a899f785270f1
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-06-07 19:20:19 +0300

    test06

---
+++ Test01/test06.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//pointer - ptr
+int main() {
+	int* ptr_array = nullptr;
+	int size;
+
+	/*cout << "Input size of array: ";
+	cin >> size;*/
+
+	cout << ptr_array << endl;
+
+	return 0;
+}
