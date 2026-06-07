1d1ca856a68ad3c5a07a8758edcd65bd99155d8d
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-06-07 19:26:01 +0300

    test07

--- Test01/test06.cpp
+++ Test01/test06.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
-
-//pointer - ptr
-int main() {
-	int* ptr_array = nullptr;
-	int size;
-
-	/*cout << "Input size of array: ";
-	cin >> size;*/
-
-	cout << ptr_array << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+////pointer - ptr
+//int main() {
+//	int* ptr_array = nullptr;
+//	int size;
+//
+//	/*cout << "Input size of array: ";
+//	cin >> size;*/
+//
+//	cout << ptr_array << endl;
+//
+//	return 0;
+//}
---
+++ Test01/test07.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+////pointer - ptr
+//int main() {
+//	int number = 100;
+//	int* ptr = &number;
+//
+//	*ptr *= 2;
+//	
+//	cout << "number = " << number << endl;
+//
+//	return 0;
+//}
